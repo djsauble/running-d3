fd3d02a4efcaab7e061c10dee6180e9b9b375373
Daniel Sauble <djsauble@gmail.com>
2016-09-01 16:44:53 -0700

    Fetch run data from CouchDB and display it

--- script.js
+++ script.js
@@ -3,24 +3,47 @@ var colors = [
     'lightblue'
 ];
 
-// Fade the background
-d3.select('body').transition()
-    .style('background-color', 'black');
+var data = [];
 
-// Update
-var div = d3.select('body')
-    .selectAll('div')
-    .data([2, 3, 5, 7, 11, 15, 17, 19])
-    .text(setText)
-    .style('background-color', setBackground);
+var url = 'http://127.0.0.1:5984/z5e4a2358-1120-4fdb-b37a-ff65ecc57f37/_all_docs?include_docs=true';
 
-// Enter
-div.enter().append('div')
-    .text(setText)
-    .style('background-color', setBackground);
+// Request data
+var request = new XMLHttpRequest()
+request.responseType = 'json';
+request.addEventListener('load', loadData);
+request.open('GET', url);
+request.send();
 
-// Exit
-div.exit().remove();
+// Load data from a request into the UI
+function loadData(event) {
+    data = parseData(event.target.response);
+    render();
+}
+
+// Parse data
+function parseData(response) {
+    return response.rows.map(function(r) {
+        return r.doc;
+    });
+}
+
+// Render data
+function render() {
+    // Update
+    var div = d3.select('body')
+        .selectAll('div')
+        .data(data)
+        .text(setText)
+        .style('background-color', setBackground);
+
+    // Enter
+    div.enter().append('div')
+        .text(setText)
+        .style('background-color', setBackground);
+
+    // Exit
+    div.exit().remove();
+}
 
 // Helper methods
 
@@ -30,5 +53,5 @@ function setBackground(d, i) {
 }
 
 function setText(d) {
-    return d;
+    return `${Math.round(d.distance / 1609.344 * 10.0) / 10.0} mi`;
 }
